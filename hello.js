8530012f2f3185e0b815a2751f109d3f50933eb5
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-05-18 23:18:56 -0500

    Completed & Tested Hello World assignment

--- hello.js
+++ hello.js
@@ -4,19 +4,38 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log("Hello World!");
 
+console.log('Hello World!')
+
+console.log(22)
+
+console.log(2+2)
+
+console.log("Hello World!\nHave a Wonderful Day!")
+
+console.log("Hello World!","Nice to Meet You!")
+
+console.log('Quoth the Raven "Nevermore"')
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
    1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
+
+   2. Figure out what the parentheses do. Will the code work without them? ---- Code causes a Syntax Error
+
    3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
-   4. Remove the semi-colon, ;.
-   5. Print a number. (Bonus: Print two numbers added together).
-   6. Print multiple messages one after the other.
+      Do we need to include both opening and closing quote marks? ---- YES, BOTH Opening & Closing Quote marks are NEEDED!!!
+      Is there a difference between using a single or a double quote (' vs. ")? ---- There is NO difference between using Single or Double quote, BUT must be consistent and Open & Close with the same quote.
+
+   4. Remove the semi-colon, ;. ---- The code still works!!!
+
+   5. Print a number. (Bonus: Print two numbers added together). ---- Use operators "+, -, /, *"
+
+   6. Print multiple messages one after the other.  ---- Add "\n" in the string to create a Line BReak
+
    7. Print two messages on the same line.
-   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore". ---- Utilize the OTHER quote either Single or Double encasing the message
    9. Other. You choose!
 */
